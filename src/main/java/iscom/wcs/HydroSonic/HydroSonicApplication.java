721a3ec7456f33d6ad3dcc70493348d5a67a11a4
soninlaw <jonsoninlaw@gmail.com>
2019-12-19 11:43:03 +0100

    Initialisation du projet

---
+++ src/main/java/iscom/wcs/HydroSonic/HydroSonicApplication.java
@@ -0,0 +1,13 @@
+package iscom.wcs.HydroSonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroSonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroSonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iscom/wcs/HydroSonic/HydroSonicApplicationTests.java
@@ -0,0 +1,13 @@
+package iscom.wcs.HydroSonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydroSonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
